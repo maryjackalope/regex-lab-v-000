1e2fcedeb4b971e7104262e53621ff9efdc53ccf
Mary Jack <maryjackinthebox@gmail.com>
2018-04-16 22:23:24 +0000

    automatically backed up by learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -23,7 +23,7 @@ def first_word_capitalized_and_ends_with_punctuation?(text)
 end
 
 def valid_phone_number?(phone)
-  if text.match(/^[[:upper:]].+[[:punct:]]$/)  == nil 
+  if phone.match(//)  == nil 
     return false 
   else 
     return true 
