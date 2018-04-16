a39b2621c1bacf99a9bb1217c9ce02204ec26b65
Mary Jack <maryjackinthebox@gmail.com>
2018-04-16 21:28:42 +0000

    automatically backed up by learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,6 +1,10 @@
 def starts_with_a_vowel?(word)
   word.match(/^[aeiou]/)
-  if word.match 
+  if word.match == nil 
+    return false 
+  else 
+    return true 
+  end
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
